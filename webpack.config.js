fff9482e67f6db0c2decd204ddd03140ad9de8e6
Mark Evans <mark@new-bamboo.co.uk>
2015-07-15 13:26:35 +0100

    react n babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,16 @@ module.exports = {
   context: __dirname + "/app",
   entry: "./app.js",
 
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"],
+      }
+    ],
+  },
+
   output: {
     filename: "app.js",
     path: __dirname + "/dist"
