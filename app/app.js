37341a0b47307d0c97a6c2824e972d9f6fcfde39
Mark Evans <mark@new-bamboo.co.uk>
2015-08-03 22:00:50 +0100

    slumby

--- app/app.js
+++ app/app.js
@@ -11,4 +11,99 @@ global.state = state
 global.React = React
 
 state.apply('init')
-React.render(<App/>, document.body)
+
+import component from './components/component'
+
+global.X = component({
+  deps: ['time'],
+  compName: 'X',
+  render () {
+    this.info()
+    return <div>
+      X {this.time()} {this._reactInternalInstance._rootNodeID}
+    </div>
+  }
+})
+
+global.D = component({
+  deps: ['time'],
+  compName: 'D',
+  render () {
+    this.info()
+    return <div>
+      D {this.time()} {this._reactInternalInstance._rootNodeID}
+    </div>
+  }
+})
+
+global.C = component({
+  deps: [],
+  compName: 'C',
+  render () {
+    this.info()
+    return <div>
+      C {this._reactInternalInstance._rootNodeID}
+      {[1,2,3].map( (i) => {
+        return <X key={i}/>
+      })}
+    </div>
+  }
+})
+
+global.B = component({
+  deps: [],
+  compName: 'B',
+  render () {
+    this.info()
+    return <div>
+      B {this._reactInternalInstance._rootNodeID}
+      <X/>
+      <C/>
+    </div>
+  }
+})
+
+global.A = component({
+  deps: ['time'],
+  compName: 'A',
+  render () {
+    this.info()
+    return <div>
+      A {this.time()} {this._reactInternalInstance._rootNodeID}
+      <X/>
+      <B/>
+    </div>
+  }
+})
+
+class Dependencies {
+  constructor () {
+    this.lookup = {}
+  }
+
+  add (component, stateAttr) {
+    if (!this.lookup[stateAttr]) this.lookup[stateAttr] = new Set()
+    this.lookup[stateAttr].add(component)
+  }
+
+  remove(component, stateAttr) {
+    this.lookup[stateAttr].remove(component)
+  }
+
+  eachDirtyComponent (callback) {
+    for (let attr in this.lookup) {
+      this.lookup[attr].forEach((component) => {
+        callback(component)
+      })
+    }
+  }
+}
+global.dependencies = new Dependencies()
+
+React.render(<A/>, document.body)
+global.update = function () {
+  React.render(React.createElement(A), document.body)
+  // dependencies.eachDirtyComponent((component) => {
+  //   component.setState({})
+  // })
+}
--- app/components/component.js
+++ app/components/component.js
@@ -1,11 +1,76 @@
 require('../polyfill')
 import React from 'react'
 
+let appState
+
+// export setAppState (state) => {
+//   appState = state
+// }
+
 export default (spec) => {
   return React.createClass(Object.assign({
 
-    appState () {
-      return 3
+    childContextTypes: {
+      message: React.PropTypes.string
+    },
+
+    contextTypes: {
+      message: React.PropTypes.string
+    },
+
+    getChildContext () {
+      return {message: "STATE IS HERE!"}
+    },
+
+    componentWillMount () {
+      //console.log(this.compName, 'componentWillMount')
+      dependencies.add(this, 'time')
+    },
+
+    componentDidMount () {
+      //console.log(this.compName, 'componentDidMount')
+    },
+
+    componentWillReceiveProps () {
+      //console.log(this.compName, 'componentWillReceiveProps')
+    },
+
+    shouldComponentUpdate () {
+      return true
+      var ret
+      if (this.deps.indexOf('time') > -1) {
+        ret = true
+      } else {
+        ret = false
+      }
+      console.log(this.compName, 'shouldComponentUpdate', ret)
+      return ret
+    },
+
+    updateComponent () {
+      console.log("HERE")
+    },
+
+    componentWillUpdate () {
+      console.log(this.compName, 'componentWillUpdate')
+    },
+
+    componentDidUpdate () {
+      //console.log(this.compName, 'componentDidUpdate')
+    },
+
+    componentWillUnmount () {
+      //console.log(this.compName, 'componentWillUnmount')
+      dependencies.remove(this, 'time')
+    },
+
+    time () {
+      var d = new Date()
+      return d.getMinutes() + ':' + d.getSeconds()
+    },
+
+    info () {
+      //console.log(this.compName, "RENDER", this._reactInternalInstance._rootNodeID)
     }
 
   }, spec))
