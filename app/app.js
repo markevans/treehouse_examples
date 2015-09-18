b39caa67c1fb0a3f7126655dc4b838f53a533879
Mark Evans <mark@new-bamboo.co.uk>
2015-09-18 16:16:02 +0100

    default to router off

--- app/app.js
+++ app/app.js
@@ -6,8 +6,8 @@ import Router from './router'
 treehouse.extendReact(React.Component.prototype)
 treehouse.actions.register(require('./actions/shared'))
 
-let router = new Router()
-treehouse.actions.do('initFromUrl', {router})
+// let router = new Router()
+// treehouse.actions.do('initFromUrl', {router})
 
 treehouse.actions.do('init')
 React.render(<App/>, document.body)
